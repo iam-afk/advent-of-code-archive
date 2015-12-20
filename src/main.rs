cb3ba671b9fc5cbc0ea366498a7f745e35840fd5
Andrey Manzharov <andreyrmg@gmail.com>
2015-12-20 15:35:28 +0300

    Completed first part

--- src/main.rs
+++ src/main.rs
@@ -2,4 +2,24 @@ use std::io;
 
 fn main() {
     let stdin = io::stdin();
+    let mut answer = 0usize;
+    let mut line = String::new();
+    while stdin.read_line(&mut line).unwrap() > 0 {
+        {
+            let s = line.trim();
+            answer += s.len() + 2;
+            let mut chars = s.chars();
+            while let Some(c) = chars.next() {
+                if c == '\\' {
+                    if let Some('x') = chars.next() {
+                        chars.next();
+                        chars.next();
+                    }
+                };
+                answer -= 1
+            }
+        }
+        line.clear();
+    }
+    println!("{}", answer);
 }
