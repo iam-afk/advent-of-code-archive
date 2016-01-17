5d5481080cf0494017b0d20b7761a09e9f100308
Andrey Manzharov <andreyrmg@gmail.com>
2016-01-17 12:00:16 +0300

    Completed second part

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,61 @@
-use std::cmp;
 use std::io;
 
 const FINISH: usize = 2503;
 
+enum State { FLYING, RESTING }
+
+use State::*;
+
+struct Reindeer {
+    speed: usize,
+    time: usize,
+    rest_time: usize,
+    state: State,
+    rest: usize,
+    position: usize,
+    points: usize
+}
+
 fn main() {
+    let mut reindeers = vec![];
     let stdin = io::stdin();
     let mut line = String::new();
-    let mut answer = 0usize;
     while stdin.read_line(&mut line).unwrap() > 0 {
         {
             let w: Vec<_> = line.split_whitespace().collect();
-            let speed = w[3].parse::<usize>().unwrap();
             let time = w[6].parse::<usize>().unwrap();
-            let rest_time = w[13].parse::<usize>().unwrap();
-
-            let t = FINISH % (time + rest_time);
-            let d = speed * (FINISH / (time + rest_time) * time + cmp::min(t, time));
-            answer = cmp::max(answer, d);
+            reindeers.push(Reindeer {
+                speed: w[3].parse::<usize>().unwrap(),
+                time: time,
+                rest_time: w[13].parse::<usize>().unwrap(),
+                state: FLYING,
+                rest: time,
+                position: 0,
+                points: 0
+            });
         }
         line.clear();
     }
+    for _ in 0..FINISH {
+        for r in reindeers.iter_mut() {
+            if r.rest == 0 {
+                match r.state {
+                    FLYING => { r.state = RESTING; r.rest = r.rest_time; },
+                    RESTING => { r.state = FLYING; r.rest = r.time; }
+                };
+            }
+            match r.state {
+                 FLYING => r.position += r.speed,
+                 _ => ()
+            }
+            r.rest -= 1;
+        }
+        reindeers.sort_by(|x, y| y.position.cmp(&x.position));
+        let leader_position = reindeers[0].position;
+        for r in reindeers.iter_mut().take_while(|r| r.position == leader_position) {
+            r.points += 1;
+        }
+    }
+    let answer = reindeers.iter().map(|r| r.points).max().unwrap();
     println!("{}", answer);
 }
