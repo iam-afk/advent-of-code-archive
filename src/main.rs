0fe85f8a3f09e66495dab9399707d5bb7bc77dc3
Andrey Manzharov <andreyrmg@gmail.com>
2017-02-11 13:41:08 +0300

    Completed first part

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,32 @@ use hyper::client::Client;
 use hyper::client::response::Response;
 use hyper::header::Cookie;
 
+fn is_tls_supported(ip: &str) -> bool {
+    let mut x = '\0';
+    let mut y = '\0';
+    let mut z = '\0';
+    let mut supported = false;
+    let mut hypernet = false;
+    for c in ip.chars() {
+        if c == '[' {
+            hypernet = true;
+        }
+        if c == ']' {
+            hypernet = false;
+        }
+        if x == c && y == z && x != y {
+            if hypernet {
+                return false;
+            }
+            supported = true;
+        }
+        x = y;
+        y = z;
+        z = c;
+    }
+    supported
+}
+
 fn fetch_input() -> BufReader<Response> {
     let mut cookie = String::from("session=");
     cookie.push_str(&env::args().nth(1).unwrap());
@@ -20,6 +46,12 @@ fn fetch_input() -> BufReader<Response> {
 }
 
 fn main() {
+    println!("{}",
+             fetch_input()
+                 .lines()
+                 .map(|s| s.unwrap())
+                 .filter(|s| is_tls_supported(&s))
+                 .count());
 }
 
 #[cfg(test)]
@@ -28,6 +60,15 @@ mod tests {
 
     #[test]
     fn examples() {
+        assert!(is_tls_supported("abba[mnop]qrst"));
+        assert!(!is_tls_supported("abcd[bddb]xyyx"));
+        assert!(!is_tls_supported("aaaa[qwer]tyui"));
+        assert!(is_tls_supported("ioxxoj[asdfgh]zxcvbn"));
+    }
+
+    #[test]
+    fn abba_within_hypernet_sequence() {
+        assert!(!is_tls_supported("abba[xyyx]abba"));
     }
 
 }
