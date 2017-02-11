62bd8147449fa7320bfca655117a86db491747fd
Andrey Manzharov <andreyrmg@gmail.com>
2017-02-11 13:39:12 +0300

    Fetch puzzle input

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,33 @@
-use std::io;
+use std::env;
+use std::io::prelude::*;
+use std::io::BufReader;
+
+extern crate hyper;
+
+use hyper::client::Client;
+use hyper::client::response::Response;
+use hyper::header::Cookie;
+
+fn fetch_input() -> BufReader<Response> {
+    let mut cookie = String::from("session=");
+    cookie.push_str(&env::args().nth(1).unwrap());
+    let client = Client::new();
+    let response = client.get("http://adventofcode.com/2016/day/7/input")
+        .header(Cookie(vec![cookie]))
+        .send()
+        .unwrap();
+    BufReader::new(response)
+}
 
 fn main() {
-    let stdin = io::stdin();
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn examples() {
+    }
+
 }
