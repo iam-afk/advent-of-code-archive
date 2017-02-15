553da061e35f7c3092458dc79d0cd8869fd80716
Andrey Manzharov <andreyrmg@gmail.com>
2017-02-15 18:45:03 +0300

    Completed first part

--- src/main.rs
+++ src/main.rs
@@ -22,9 +22,43 @@ fn fetch_input() -> BufReader<Response> {
 }
 
 fn main() {
-    for line in fetch_input().lines() {
-        println!("{:?}", line);
+    let mut file = String::new();
+    fetch_input().read_to_string(&mut file).unwrap();
+    println!("{}", decompressed_length(&file));
+}
+
+fn decompressed_length(file: &str) -> usize {
+    let mut chars = file.chars();
+    let mut count = 0usize;
+    loop {
+        match chars.next() {
+            None => break,
+            Some(x) if x.is_whitespace() => (),
+            Some('(') => {
+                let mut subsequent_count = 0usize;
+                while let Some(d) = chars.next() {
+                    if d == 'x' {
+                        break;
+                    }
+                    subsequent_count = subsequent_count * 10 + (d as usize - '0' as usize);
+                }
+                let mut times = 0usize;
+                while let Some(d) = chars.next() {
+                    if d == ')' {
+                        break;
+                    }
+                    times = times * 10 + (d as usize - '0' as usize);
+                }
+                count += subsequent_count * times;
+                for _ in 0..subsequent_count {
+                    chars.next();
+                }
+
+            }
+            _ => count += 1,
+        }
     }
+    count
 }
 
 #[cfg(test)]
@@ -32,6 +66,13 @@ mod tests {
     use super::*;
 
     #[test]
-    fn examples() {}
+    fn examples() {
+        assert_eq!(decompressed_length("ADVENT"), 6);
+        assert_eq!(decompressed_length("A(1x5)BC"), 7);
+        assert_eq!(decompressed_length("(3x3)XYZ"), 9);
+        assert_eq!(decompressed_length("A(2x2)BCD(2x2)EFG"), 11);
+        assert_eq!(decompressed_length("(6x1)(1x3)A"), 6);
+        assert_eq!(decompressed_length("X(8x2)(3x3)ABCY"), 18);
+    }
 
 }
