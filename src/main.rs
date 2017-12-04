bb2e096c5d1510456a354939672eea627b081d09
Andrey Manzharov <andreyrmg@gmail.com>
2017-12-04 19:41:20 +0300

    improve basic template

--- src/main.rs
+++ src/main.rs
@@ -42,7 +42,9 @@ where
 }
 
 fn main() {
-    with_day_input(2017, ???, |input| input).unwrap();
+    with_day_input(2017, compile_error!("put day number here"), |input| {
+        input.to_string()
+    }).unwrap();
 }
 
 #[cfg(test)]
