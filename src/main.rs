39b93e5a2e5140955ab70f3105824133a857f1f0
Andrey Manzharov <andreyrmg@gmail.com>
2018-12-02 09:38:11 +0300

    got first star

--- src/main.rs
+++ src/main.rs
@@ -14,11 +14,41 @@ fn main() -> Result<(), io::Error> {
 }
 
 fn first_star(input: &str) -> impl fmt::Display {
-    input.to_string()
+    let v: Vec<_> = input.lines().collect();
+    checksum(&v)
 }
 
-fn second_star(input: &str) -> impl fmt::Display {
-    input.to_string()
+fn second_star(_input: &str) -> impl fmt::Display {
+    ""
+}
+
+fn checksum(ids: &[&str]) -> usize {
+    let mut two = 0usize;
+    let mut three = 0usize;
+
+    for id in ids {
+        let mut letters = [0usize; 26];
+        for c in id.chars() {
+            letters[c as usize - 'a' as usize] += 1;
+        }
+        let mut exactly_two = false;
+        let mut exactly_three = false;
+        for &count in &letters {
+            if count == 2 {
+                exactly_two = true
+            }
+            if count == 3 {
+                exactly_three = true
+            }
+        }
+        if exactly_two {
+            two += 1
+        }
+        if exactly_three {
+            three += 1
+        }
+    }
+    two * three
 }
 
 #[cfg(test)]
@@ -26,6 +56,11 @@ mod tests {
     use super::*;
 
     #[test]
-    fn examples() {}
+    fn examples() {
+        assert_eq!(
+            12,
+            checksum(&["abcdef", "bababc", "abbcde", "abcccd", "aabcdd", "abcdee", "ababab",])
+        );
+    }
 
 }
