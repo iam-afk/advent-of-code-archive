f4dfba1216c487c68fe74bfdc430a7a8838a07d8
Andrey Manzharov <andreyrmg@gmail.com>
2018-12-05 19:26:05 +0300

    do tricky comparision between lower and upper ascii letters

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,10 @@ use std::io::prelude::*;
 
 fn main() -> Result<(), io::Error> {
     let mut stdin = io::stdin();
-    let mut input = String::new();
-    stdin.read_to_string(&mut input)?;
+    let mut buffer = String::new();
+    stdin.read_to_string(&mut buffer)?;
 
+    let input = buffer.trim_end();
     println!(" *: {}", first_star(&input));
     println!("**: {}", second_star(&input));
 
@@ -14,11 +15,11 @@ fn main() -> Result<(), io::Error> {
 }
 
 fn first_star(input: &str) -> impl fmt::Display {
-    react(input.trim().bytes())
+    react(input.bytes())
 }
 
 fn second_star(input: &str) -> impl fmt::Display {
-    shortest(input.trim())
+    shortest(input)
 }
 
 fn react<C>(polymer: C) -> usize
@@ -28,7 +29,7 @@ where
     let mut v = vec![];
     for unit in polymer {
         let &previous = v.last().unwrap_or(&0u8);
-        if unit != previous && unit.to_ascii_lowercase() == previous.to_ascii_lowercase() {
+        if unit ^ 0x20 == previous {
             v.pop();
         } else {
             v.push(unit);
@@ -39,7 +40,7 @@ where
 
 fn shortest(polymer: &str) -> usize {
     ('a' as u8..='z' as u8)
-        .map(|x| react(polymer.bytes().filter(|c| c.to_ascii_lowercase() != x)))
+        .map(|x| react(polymer.bytes().filter(|c| c | 0x20 != x)))
         .min()
         .unwrap()
 }
