8c95f9c21c073e5789850d71097d7961aedfaf4c
Andrey Manzharov <andreyrmg@gmail.com>
2018-12-06 20:58:00 +0300

    got first star

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
+use std::collections::HashMap;
 use std::fmt;
 use std::io;
 use std::io::prelude::*;
+use std::num;
+use std::str;
 
 fn main() -> Result<(), io::Error> {
     let mut stdin = io::stdin();
@@ -14,12 +17,57 @@ fn main() -> Result<(), io::Error> {
     Ok(())
 }
 
-fn first_star(_input: &str) -> impl fmt::Display {
-    ""
+fn first_star(input: &str) -> impl fmt::Display {
+    let points: Vec<_> = input.lines().map(|line| line.parse().unwrap()).collect();
+    largest(&points)
 }
 
 fn second_star(_input: &str) -> impl fmt::Display {
     ""
+}
+
+#[derive(Hash, PartialEq, Eq)]
+struct Point(i32, i32);
+
+impl str::FromStr for Point {
+    type Err = num::ParseIntError;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut v = s.splitn(2, ", ").map(|s| s.parse::<i32>().unwrap());
+        let row = v.next().unwrap();
+        let col = v.next().unwrap();
+        Ok(Point(row, col))
+    }
+}
+
+fn largest(points: &[Point]) -> u32 {
+    let mut areas = HashMap::new();
+    for point in points.iter() {
+        areas.insert(point, 0u32);
+    }
+
+    let min_row = points.iter().map(|p| p.0).min().unwrap();
+    let max_row = points.iter().map(|p| p.0).max().unwrap();
+    let min_col = points.iter().map(|p| p.1).min().unwrap();
+    let max_col = points.iter().map(|p| p.1).max().unwrap();
+
+    for r in min_row..=max_row {
+        for c in min_col..=max_col {
+            let mut v: Vec<_> = points
+                .iter()
+                .map(|p| (p, (p.0 - r).abs() + (p.1 - c).abs()))
+                .collect();
+            v.sort_by_key(|&(_, d)| d);
+            if v[0].1 < v[1].1 {
+                if r == min_row || r == max_row || c == min_col || c == max_col {
+                    areas.remove(v[0].0);
+                } else {
+                    areas.entry(&v[0].0).and_modify(|e| *e += 1);
+                }
+            }
+        }
+    }
+
+    *areas.values().max().unwrap()
 }
 
 #[cfg(test)]
@@ -27,6 +75,16 @@ mod tests {
     use super::*;
 
     #[test]
-    fn examples() {}
+    fn examples() {
+        let points = vec![
+            Point(1, 1),
+            Point(1, 6),
+            Point(8, 3),
+            Point(3, 4),
+            Point(5, 5),
+            Point(8, 9),
+        ];
+        assert_eq!(17, largest(&points));
+    }
 
 }
